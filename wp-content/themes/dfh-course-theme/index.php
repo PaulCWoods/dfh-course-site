eb82e89699a4ac94b102b32ee2e985e59ad67098
PaulCWoods <pwoods5@icloud.com>
2026-08-21 00:27:08 +0100

    Log out added to homescreen

--- wp-content/themes/dfh-course-theme/index.php
+++ wp-content/themes/dfh-course-theme/index.php
@@ -20,32 +20,42 @@ get_header();
     <!-- Hero Section -->
     <section class="home-hero">
         <div class="container">
-            <?php
-            // Determine the target URL for the hero button: prefer the user's active course when available.
-            $hero_target = home_url( '/course/' );
-            if ( is_user_logged_in() && function_exists( 'dfh_get_student_current_lesson' ) ) {
-                $active_lesson = dfh_get_student_current_lesson();
-                if ( is_int( $active_lesson ) && $active_lesson > 0 && function_exists( 'dfh_get_courses_for_lesson' ) ) {
-                    $courses = dfh_get_courses_for_lesson( $active_lesson );
-                    if ( ! empty( $courses ) ) {
-                        $hero_target = get_permalink( (int) $courses[0] );
+                <?php
+                // Determine the target URL for the hero button: prefer the user's active course when available.
+                $hero_target = home_url('/course/');
+                if (is_user_logged_in() && function_exists('dfh_get_student_current_lesson')) {
+                    $active_lesson = dfh_get_student_current_lesson();
+                    $current_user = wp_get_current_user();
+                    $user_name = '';
+                    if ($current_user && $current_user->ID) {
+                        $user_name = $current_user->display_name ? $current_user->display_name : $current_user->user_login;
+                    }
+                    if (is_int($active_lesson) && $active_lesson > 0 && function_exists('dfh_get_courses_for_lesson')) {
+                        $courses = dfh_get_courses_for_lesson($active_lesson);
+                        if (!empty($courses)) {
+                            $hero_target = get_permalink((int) $courses[0]);
+                        }
                     }
                 }
-            }
 
-            if ( is_user_logged_in() ): ?>
-                <a href="<?php echo esc_url( $hero_target ); ?>" class="button">
-                    Resume your course
-                    <svg class="icon dir" width="32" height="32" aria-hidden="true">
-                        <use href="#ArrowRight" />
-                    </svg>
-                </a>
-            <?php else: ?>
+                if (is_user_logged_in()): ?>
+                <p class="home-hero__logged-in title">Welcome back, <?php echo esc_html($user_name); ?></p>
+                <div class="home-hero__access">
+                    <a href="<?php echo esc_url($hero_target); ?>" class="button strong">
+                        Resume your course
+                        <svg class="icon dir" width="32" height="32" aria-hidden="true">
+                            <use href="#ArrowRight" />
+                        </svg>
+                    </a>
+                    <a class="button" href="<?php echo esc_url(wp_logout_url(home_url())); ?>">Log Out</a>
+                </div>
+                <?php else: ?>
+
                 <div class="home-hero__access">
                     <a href="<?php echo esc_url(home_url('/register/')); ?>" class="button strong">Get Started</a>
                     <a href="<?php echo esc_url(home_url('/login/')); ?>" class="button">Log In</a>
                 </div>
-            <?php endif; ?>
+                <?php endif; ?>
         </div>
     </section>
 
@@ -65,33 +75,33 @@ get_header();
 
             if ($courses_query->have_posts()):
                 ?>
-                <ul class="course-grid">
-                    <?php while ($courses_query->have_posts()):
-                        $courses_query->the_post(); ?>
-                        <li class="course-grid__item">
-                            <div class="course-card">
-                                <?php if (has_post_thumbnail()): ?>
-                                    <div class="course-card__thumb">
-                                        <a href="<?php the_permalink(); ?>">
-                                            <?php the_post_thumbnail('medium_large'); ?>
-                                        </a>
-                                    </div>
-                                <?php endif; ?>
-                                <div class="course-card__content">
-                                    <a class="course-card__link link" href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
-                                    <?php the_excerpt(); ?>
-                                    <?php if ( function_exists('dfh_user_has_course_access') && dfh_user_has_course_access() ): ?>
-                                        <span class="course-card__badge enrolled badge">Enroled</span>
-                                    <?php endif; ?>
+                    <ul class="course-grid">
+                        <?php while ($courses_query->have_posts()):
+                            $courses_query->the_post(); ?>
+                                <li class="course-grid__item">
+                                    <div class="course-card">
+                                        <?php if (has_post_thumbnail()): ?>
+                                                <div class="course-card__thumb">
+                                                    <a href="<?php the_permalink(); ?>">
+                                                        <?php the_post_thumbnail('medium_large'); ?>
+                                                    </a>
+                                                </div>
+                                        <?php endif; ?>
+                                        <div class="course-card__content">
+                                            <a class="course-card__link link" href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
+                                            <?php the_excerpt(); ?>
+                                            <?php if (function_exists('dfh_user_has_course_access') && dfh_user_has_course_access()): ?>
+                                                    <span class="course-card__badge enrolled badge">Enroled</span>
+                                            <?php endif; ?>
                                     
-                                </div>
-                            </div>
-                        </li>
-                    <?php endwhile;
-                    wp_reset_postdata(); ?>
-                </ul>
+                                        </div>
+                                    </div>
+                                </li>
+                        <?php endwhile;
+                        wp_reset_postdata(); ?>
+                    </ul>
             <?php else: ?>
-                <p>No courses are currently published. Check back soon!</p>
+                    <p>No courses are currently published. Check back soon!</p>
             <?php endif; ?>
 
         </div>
